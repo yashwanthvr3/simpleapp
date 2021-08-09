d36261405512db42b83930c9a93a7d0337b5bc3a
yashwanthvr3 <84366519+yashwanthvr3@users.noreply.github.com>
2021-08-09 11:08:41 +0530

    initial commit

---
+++ SimpleAPP/Controlers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SimpleAPP.Models;
+namespace SimpleAPP.Controlers
+{
+    [RoutePrefix("api")]
+    public class ValuesController : ApiController
+    {
+
+        [Route("values")]
+        [HttpGet]
+        public IHttpActionResult GetValues()
+        {
+            var resulit = new string[]
+            {
+                "Ախ ոտս",
+                "Տեղ տուր նստեմ"
+            };
+            return Ok(resulit);
+        }
+
+        [Route("values")]
+        [HttpPost]
+        public IHttpActionResult SendValues(Person model)
+        {
+            if (model==null)
+            {
+                return BadRequest();
+            }
+            model.Age++;
+            model.Name += "!";
+            return Ok(model);
+        }
+        //Get Chrome extensin Postman
+    }
+}
